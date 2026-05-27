00e5a634afd673a41b9c4e92f757fdda15055a4e
maike1983 <Maikevandartel@outlook.com>
2026-05-27 09:39:05 +0200

    t/m Opdracht 2, alle posts nu vanuit State

--- src/App.jsx
+++ src/App.jsx
@@ -17,11 +17,10 @@ function App() {
                 <Route path="/post-plaatsen" element={<NewPost/>} />
                 <Route path="/blogoverzicht" element={<AllBlogs/>} />
                 <Route path="*" element={<NotFound/>} />
-                <Route path="/posts/:id" element={<PostDetails />}/>
+                <Route path="/blogs/:id" element={<PostDetails />}/>
             </Routes>
         </div>
     )
 }
-
 
 export default App
--- src/pages/all-blogs/AllBlogs.jsx
+++ src/pages/all-blogs/AllBlogs.jsx
@@ -1,18 +1,44 @@
 import "./AllBlogs.css"
-import posts from "../../constants/data.json"
 import {Link} from "react-router-dom";
+import axios from "axios";
+import {useEffect, useState} from "react";
 
 function AllBlogs() {
-    console.log(posts);
+// npm
+    const [blogs, setBlogs] = useState([]);
+    const [error, setError] = useState("");
+
+    const endpoint="https://novi-backend-api-wgsgz.ondigitalocean.app/api/blogposts";
+    async function fetchBlogPosts() {
+        try {
+            const result=await axios.get(endpoint, {
+                headers: {
+                    "novi-education-project-id": "d17b3fdb-9491-4065-b047-efe9ea4b773c"
+                }
+            });
+            // console.log(result.data);
+            setBlogs(result.data);
+        } catch (e) {
+            console.error(e)
+            setError("Helaas, er ging iets mis.");
+        }
+    }
+
+    useEffect(() => {
+        fetchBlogPosts();
+    },[]);
 
     return (
         <>
-            <h2>Totale hoeveelheid blogs: {posts.length} </h2>
+            {error && <p>{error}</p>}
+
+            {blogs?.length > 0 && <h2>Totale hoeveelheid blogs: {blogs.length}</h2>}
+            {/*<button type="button" onClick={fetchBlogPosts}>Haal blogposts op</button>*/}
             <div className="card-section">
-            {posts.map((post) => (
-                <article className="blog-card" key={post.id}>
-                    <h2><Link to={`/posts/${post.id}`}>{post.title}</Link></h2>
-                    <p>{post.comments} reacties - {post.shares} keer gedeeld</p>
+                {blogs.length > 0 && blogs.map((blog) => (
+                <article className="blog-card" key={blog.id}>
+                    <h2><Link to={`/blogs/${blog.id}`}>{blog.title}</Link></h2>
+                    <p>{blog.comments} reacties - {blog.shares} keer gedeeld</p>
                 </article>
             ))}
             </div>
